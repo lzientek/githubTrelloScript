718dd2d8b6fe4f60b1e061e27dda1fdf529e3e97
lucas zientek <152565@supinfo.com>
2017-07-22 14:40:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // ==UserScript==
 // @name         PR Github Trello
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
+// @updateUrl    https://gist.github.com/lzientek/698a66f28878dddcfcb8324a9cbd3f11/raw/cc816eeda07b2ff522727e677a955e3b6fce3696/github-trello-mistertemp.js
+// @downloadUrl  https://gist.github.com/lzientek/698a66f28878dddcfcb8324a9cbd3f11/raw/cc816eeda07b2ff522727e677a955e3b6fce3696/github-trello-mistertemp.js
 // @description  try to take over the world!
-// @author       You
+// @author       lzientek
 // @match        https://github.com/*
-// @grant        none
+// @connect      trello.com
+// @grant        GM_xmlhttpRequest
+// @grant        GM_registerMenuCommand
 // @require      http://code.jquery.com/jquery-1.7.1.min.js
 // @require      https://api.trello.com/1/client.js?key=c2193277e987e0f5f33523324b3ae260
 // @require      https://gist.githubusercontent.com/lzientek/c39a8adf8d71804b99aab6b7cad7b2ae/raw/63cbc8dbcd8fefb5d09a379274bf8750fb82f78e/co-browser.js
@@ -14,11 +18,12 @@
 
 
 const asyncTrello = function* (verb, url, body) {
-    if(body) {
-        yield new Promise((resolve, reject) => Trello[verb](url, body, resolve, reject));
-    } else {
-        yield new Promise((resolve, reject) => Trello[verb](url, resolve, reject));
-    }
+    return yield new Promise((resolve, reject) => GM_xmlhttpRequest({
+        url: `https://api.trello.com/1${url}?key=${Trello.key()}&token=${Trello.token()}`,
+        method: verb,
+        fetch: fetch,
+        onload: (result) =>{ resolve(JSON.parse(result.response));},
+    }));
 };
 
 const asyncAuthorize = function* () {
@@ -29,8 +34,8 @@ const asyncAuthorize = function* () {
             read: 'true',
             write: 'true' },
         expiration: 'never',
-        success: () => {
-            console.log('trello connected');
+        success: (result) => {
+            console.log('trello connected', result);
             resolve();
         },
         error:  function() {console.log('Connection to trello Failed'); reject();}
@@ -45,14 +50,41 @@ const listIds = {v1: {
 }};
 
 const generateAndSetNewTitle = (currentId, prType, text) => {
-    text= /\(\d+\)/.replace(text, '');
+    text= text.replace(/\(\d+\)/, '').trim();
     let newVal = `[WIP] ${prType} ${currentId} - ${text}`;
 
     $('#pull_request_title').val(newVal);
     return newVal;
 };
+
+const generateAndSetGlobalText = (card, prType) => {
+    const hasFrontend = card.labels.some(val => val.color === 'orange');
+    const hasBackend = card.labels.some(val => val.color === 'yellow');
+
+    let value = `## Description
+${card.desc}
+### Trello url
+${card.url}
+## Motivation and Context
+\`Why is this change required? What problem does it solve?\`
+## How Has This Been Tested?
+${hasFrontend ? `
+- [ ] Frontend component and container tests.
+- [ ] Frontend e2e tests.` :''}
+${hasBackend ? `- [ ] Routes tests.` :''}
+${hasBackend && hasFrontend ? `- [ ] Reducer, actions and services tests.` :''}
+## Types of changes
+- [${prType ==='FEATURE'? 'x' : ' '}] Feature
+- [${prType ==='BUGFIX'? 'x' : ' '}] Bugfix
+- [${prType ==='HOTFIX'? 'x' : ' '}] Hotfix
+## Checklist:
+- [ ] My code follows the code style of this project.
+- [ ] I have added tests to cover my changes.
+- [ ] All new and existing tests passed.`;
+    $('#pull_request_body').val(value);
+};
+
 (function() {
-    console.log('yes');
     co(function* () {
         let version = null;
         const splittedUrl = window.location.href.split('/');
@@ -65,7 +97,7 @@ const generateAndSetNewTitle = (currentId, prType, text) => {
         yield asyncAuthorize();
 
         if(splittedUrl.length >= 8 & splittedUrl[5] === 'compare')  { // compare mode
-            const currentId = /^\d+/.exec(splittedUrl[7]);
+            const currentId = parseInt(/^\d+/.exec(splittedUrl[7])[0]);
             const prType = splittedUrl[6].indexOf('...') >= 0 ? splittedUrl[6].split('...')[1].toUpperCase() : splittedUrl[6].toUpperCase();
 
             if(!currentId) { return; }
@@ -73,15 +105,12 @@ const generateAndSetNewTitle = (currentId, prType, text) => {
             const cards = yield asyncTrello('get', `/boards/${idBoard}/cards`);
             const currentCard = cards.find(val => val.idShort === currentId);
 
-
+            generateAndSetNewTitle(currentId, prType, currentCard.name);
+            generateAndSetGlobalText(currentCard, prType);
+            $('.btn-link.muted-link.js-issue-assign-self').click(); // auto assign
+            console.log(currentCard);
         } else if(splittedUrl.length === 7 && splittedUrl[6] === 'pull') { // PR mode
             // pull request mode
         }
-
-
-
-        const backlog = yield asyncTrello('get', '/boards/');
-        const cards = yield asyncTrello('get', `/boards/${idBoard}/cards`);
-        const currentCard = cards.find(val => val.idShort === currentId);
     }).then(() => console.log('youpi')).catch(console.log);
 })();
